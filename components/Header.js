b1b39d8b467408d4229f1f3f891e6b49520fd2b9
meekeboose <meekeboose@gmail.com>
2020-06-22 11:15:48 +0800

    fresh _app _document nextconfig

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h2>This is the header area</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const about = () => {
+  return (
+    <div>
+      <h2>This is the About Page</h2>
+    </div>
+  );
+};
+
+export default about;
